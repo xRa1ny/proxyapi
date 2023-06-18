9dacd7815b1bb394c50ac1a8dd020248caedb529
xRa1ny <mikeneumann2302@gmail.com>
2023-06-18 15:42:47 +0200

    major functional command changes
    - added pattern support for var arg type command parameters (%PARAM%*)

--- src/main/java/me/xra1ny/proxyapi/models/command/RCommand.java
+++ src/main/java/me/xra1ny/proxyapi/models/command/RCommand.java
@@ -53,7 +53,7 @@ public abstract class RCommand extends Command implements TabExecutor {
     }
 
     /**
-     * called when this command is executed with only the base command (/commandname)
+     * called when this command is executed with only the base command (/command)
      * @param sender the sender
      * @return the status of this command execution
      */
@@ -61,7 +61,7 @@ public abstract class RCommand extends Command implements TabExecutor {
     protected abstract CommandReturnState executeBaseCommand(@NotNull CommandSender sender) throws Exception;
 
     /**
-     * called when this command is executed with arguments (/command name arg1 arg2 arg3)
+     * called when this command is executed with arguments (/command arg1 arg2 arg3)
      * @param sender the sender
      * @param args the arguments
      * @param values the values of any unknown arguments
@@ -112,9 +112,44 @@ public abstract class RCommand extends Command implements TabExecutor {
                 }
             }
 
-            // Create List of all valid Command Arguments in all lower case
-            final List<String> commandArgs = new ArrayList<>(Stream.of(this.args).map(String::toLowerCase).toList());
+            for(String arg : this.args) {
+                final String[] originalArgs = arg.split(" ");
+                final String[] editedArgs = originalArgs.clone();
 
+                if(originalArgs.length >= args.length) {
+                    for(int i = 0; i < args.length; i++) {
+                        if (!originalArgs[i].startsWith("%") && !originalArgs[i].endsWith("%") && !originalArgs[i].endsWith("*")) {
+                            continue;
+                        }
+
+                        editedArgs[i] = args[i];
+                    }
+                }
+            }
+
+            boolean varArg = false;
+            int varArgs = 0;
+            int varArgsIndex = Integer.MIN_VALUE;
+
+            for(String arg : this.args) {
+                final List<String> originalArgsList = List.of(arg.split(" "));
+
+                if(originalArgsList.size() >= args.length || originalArgsList.get(originalArgsList.size()-1).endsWith("%*")) {
+                    final String finalArg = originalArgsList.get(originalArgsList.size()-1);
+
+                    if(List.of(args).size() >= originalArgsList.size() && finalArg.endsWith("%*")) {
+                        varArg = true;
+
+                        if(varArgsIndex == Integer.MIN_VALUE) {
+                            varArgsIndex = originalArgsList.size()-1;
+                        }
+                    }
+                }
+            }
+
+            final List<String> commandArgs = new ArrayList<>(Stream.of(this.args)
+                    .map(String::toLowerCase)
+                    .toList());
             final List<String> commandValues = new ArrayList<>();
             final StringBuilder builder = new StringBuilder();
 
@@ -130,11 +165,17 @@ public abstract class RCommand extends Command implements TabExecutor {
                 if(contains) {
                     builder.append(builder.length() > 0 ? " " : "").append(arg);
                 }else {
-                    builder.append(builder.length() > 0 ? " " : "").append("?");
+                    if(!varArg || varArgs == 0) {
+                        builder.append(builder.length() > 0 ? " " : "").append("?");
+                    }
+
+                    if(List.of(args).indexOf(arg) >= varArgsIndex) {
+                        builder.append(varArg && varArgs == 0 ? "*" : "");
+                        varArgs++;
+                    }
+
                     commandValues.add(arg);
                 }
-
-                contains = false;
             }
 
             final CommandReturnState commandReturnState;
@@ -173,7 +214,11 @@ public abstract class RCommand extends Command implements TabExecutor {
     }
 
     @NotNull
-    public final Iterable<String> onTabComplete(@NotNull CommandSender sender, @NotNull String[] args) {
+    public abstract List<String> onCommandTabComplete(@NotNull CommandSender sender, @NotNull String args);
+
+    @NotNull
+    @Override
+    public final Iterable<String> onTabComplete(@NotNull CommandSender sender, @NotNull String @NotNull [] args) {
         final List<String> tabCompleted = new ArrayList<>();
 
         if(args.length == 1) {
@@ -184,20 +229,27 @@ public abstract class RCommand extends Command implements TabExecutor {
             final String[] originalArgs = arg.split(" ");
             final String[] editedArgs = originalArgs.clone();
 
-            if(originalArgs.length > args.length-1) {
+            if(originalArgs.length >= args.length || originalArgs[originalArgs.length-1].endsWith("%*")) {
                 for(int i = 0; i < args.length; i++) {
-                    if (!originalArgs[i].equals("%PLAYER%") &&
-                            !originalArgs[i].equals("%BOOLEAN%")) {
+                    final String originalArg = i >= originalArgs.length ? originalArgs[originalArgs.length-1] : originalArgs[i];
+
+                    if (!originalArg.startsWith("%") && !(originalArg.endsWith("%") || originalArg.endsWith("%*"))) {
                         continue;
                     }
 
-                    editedArgs[i] = args[i];
+                    editedArgs[i >= editedArgs.length ? editedArgs.length-1 : i] = args[i];
                 }
 
-                final String finalArg = originalArgs[args.length-1];
+                final String finalArg = originalArgs[args.length-1 >= originalArgs.length ? originalArgs.length-1 : args.length-1];
 
                 if(!String.join(" ", editedArgs).startsWith(String.join(" ", args))) {
                     continue;
+                }
+
+                if(finalArg.startsWith("%") && finalArg.endsWith("%*")) {
+                    tabCompleted.add("<" + finalArg.replace("%", "").replace("%*", "") + ">");
+
+                    break;
                 }
 
                 if(finalArg.equalsIgnoreCase(RPlugin.getInstance().PLAYER_IDENTIFIER)) {
@@ -208,18 +260,40 @@ public abstract class RCommand extends Command implements TabExecutor {
 
                         tabCompleted.add(player.getName());
                     }
-                }else if(finalArg.equalsIgnoreCase("%NUMBER%")) {
+                }else if(finalArg.startsWith("%NUMBER%")) {
                     tabCompleted.add("0");
-                }else if(finalArg.equalsIgnoreCase("%BOOLEAN%")) {
+                }else if(finalArg.startsWith("%BOOLEAN%")) {
                     tabCompleted.add("<true|false>");
-                }else if(finalArg.startsWith("%") && finalArg.endsWith("%")) {
-                    tabCompleted.add("<" + finalArg.replaceAll("%", "") + ">");
+                }else if(finalArg.startsWith("%") && (finalArg.endsWith("%") || finalArg.endsWith("%*"))) {
+                    tabCompleted.add("<" + finalArg.replace("%", "").replace("%*", "") + ">");
                 }else {
                     tabCompleted.add(finalArg);
                 }
-
             }
         }
+
+        final List<String> commandArgs = new ArrayList<>(Stream.of(this.args)
+                .map(String::toLowerCase)
+                .toList());
+        final StringBuilder builder = new StringBuilder();
+
+        for(String arg : args) {
+            boolean contains = false;
+
+            for(String commandArg : commandArgs) {
+                if(Arrays.asList(commandArg.split(" ")).contains(arg.toLowerCase())) {
+                    contains = true;
+                }
+            }
+
+            if(contains) {
+                builder.append(builder.length() > 0 ? " " : "").append(arg);
+            }else {
+                builder.append(builder.length() > 0 ? " " : "").append("?");
+            }
+        }
+
+        tabCompleted.addAll(onCommandTabComplete(sender, builder.toString()));
 
         return tabCompleted;
     }
